7ae25a7c71dd3342bcae73af66d1709442f71837
Carlos Badenes <cbadenes@gmail.com>
2015-09-21 11:25:25 +0200

    refactor class by name

--- w2v-wiki.sh
+++ w2v-wiki.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export MAVEN_OPTS="-Xmx112g"
 now=$(date +"%Y%m%d-%H%M%S")
-mvn exec:java -Dexec.mainClass="Word2VecModel" -Dexec.args="8 11g /home/cbadenes/test/wikipedia-dump/articles_body.csv text/model/w2v-wiki" -Dexec.classpathScope="test" > log-$now 2>&1 &
+mvn exec:java -Dexec.mainClass="w2v.ModelBuilder" -Dexec.args="8 11g /home/cbadenes/test/wikipedia-dump/articles_body.csv text/model/w2v-wiki" -Dexec.classpathScope="test" > log-$now 2>&1 &
